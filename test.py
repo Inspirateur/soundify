1452a75e70655420e60f18db5ab6990c47500444
chrisjanaqi <25613312+chrisjanaqi@users.noreply.github.com>
2017-09-24 19:45:53 +0200

    testing numpy fft

--- test.py
+++ test.py
@@ -22,17 +22,30 @@ for i, (x, y) in enumerate(hilbert(n)):
     break
 
 time = np.linspace(0, len(values) / 44100, num=len(values))
-sound = np.sin(440 / 2 * np.pi * time)
+sound = .5 * np.sin(2 * np.pi * 440 * time)
 
 
 plt.figure(0)
-plt.plot(time, sound)
+plt.plot(time, sound, marker='+')
 
-spectrum = np.fft.rfft(sound)
-for e in spectrum:
-    if np.absolute(e) > 100. :
-        print(e)
-# freq = np.linspace(44100 / len(values), 2*44100, num=len(spectrum))
-# plt.figure(1)
-# plt.semilogx(freq, spectrum, marker="+")
-# plt.show()
+fsound = np.fft.rfft(sound, len(sound))
+spectrum = np.absolute(fsound)
+
+freq = np.linspace(44100 / len(values), 44100, num=len(spectrum))
+plt.figure(1)
+plt.semilogx(freq, spectrum, marker="+")
+
+sample = np.fft.irfft(spectrum, len(fsound))
+
+time = np.linspace(0, len(sample) / 44100, num=len(sample))
+plt.figure(2)
+plt.plot(time, sample, marker='+')
+
+
+spectrum = np.absolute(np.fft.rfft(sound, len(sound)))
+freq = np.linspace(44100 / len(values), 44100, num=len(spectrum))
+plt.figure()
+plt.semilogx(freq, spectrum, marker="+")
+
+plt.show()
+
