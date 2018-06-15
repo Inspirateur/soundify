beacf91bb1294eba528c10650e00311574b1851f
chrisjanaqi <25613312+chrisjanaqi@users.noreply.github.com>
2018-06-15 14:47:11 +0200

    working conversion for Color->Number
    Still need to use np.where() to avoid NaN and infinity problems.

--- colors.py
+++ colors.py
@@ -10,10 +10,28 @@ def rgb2hsl(rgb):
     """
     convert array of rgb values into array of hsl values
     """
+    # getting components independentely
+    red, green, blue = rgb.T
+    # basic algorithm to calculate hsl values from rgb numpy compatible
     mins, maxes = np.min(rgb, axis=1), np.max(rgb, axis=1)
+    light = (mins + maxes) / 2.
+    delta = maxes - mins
 
-    hsl = rgb
-    return hsl
+    masks = np.array([None for _ in range(4)])
+    masks[0] = (delta != 0.)
+    for i in range(1, 4):
+        masks[i] = (rgb.T[i-1] == maxes)
+
+    hue = np.array(
+        masks[0] * 60. * (
+            masks[1] * (((green - blue) / delta) % 6.)
+            + masks[2] * ((blue - red) / delta + 2.)
+            + masks[3] * ((red - green)/ delta + 4.)
+        )
+    )
+
+    sat = masks[0] * delta / (1. - np.abs(2. * light - 1.))
+    return np.array((hue, sat, light)).T
 
 
 def hsl2rgb(hsl):
@@ -33,12 +51,12 @@ def hsl2rgb(hsl):
         masks[i] = (i <= hprime) & (hprime < i + 1)
 
     # calculating each component reffering to wikipedia tables
-    R1 = np.array(C * (masks[0] | masks[5])
-                  + X * (masks[1] | masks[4]))
-    G1 = np.array(C * (masks[1] | masks[2])
-                  + X * (masks[0] | masks[3]))
-    B1 = np.array(C * (masks[3] | masks[4])
-                  + X * (masks[2] | masks[5]))
+    R1 = np.array((masks[0] | masks[5]) * chroma
+                  + (masks[1] | masks[4]) * X)
+    G1 = np.array((masks[1] | masks[2]) * chroma
+                  + (masks[0] | masks[3]) * X)
+    B1 = np.array((masks[3] | masks[4]) * chroma
+                  + (masks[2] | masks[5]) * X)
     m = light - 1./2. * chroma
 
     rgb = np.array((R1 + m, G1 + m, B1 + m)).T
@@ -47,16 +65,14 @@ def hsl2rgb(hsl):
 
 def color2number(colors):
     """
-    take a color (triplet of float between 0 and 1), and return the complex
+    take a color (triplet of float in [0, 1]), and return the complex
     number in the unit disk associated to it
     """
     # colors are floats in [0, 1]
-    assert ((0 <= colors) & (colors <= 1)).all()
+    assert ((colors >= 0.) & (colors <= 1.)).all()
     # grey scale
-    hsl = rgb2hsl(colors)
-    modules = 1/3 * np.sum(colors, axis=1)
-    angles = colorWheel(colors)
-    return modules * np.exp(1j*angles)
+    hue, sat, light = rgb2hsl(colors).T
+    return light * np.exp(1j*(hue * np.pi / 180.))
 
 
 def number2color(numbers):
@@ -65,7 +81,7 @@ def number2color(numbers):
     associated to it
     """
     #  we don't want any amplitude greater to 1
-    assert (np.abs(numbers) <= 1).all()
+    assert (np.abs(numbers) <= 1.).all()
 
     # generating hsl values for each complex numbers
     hsl = np.array((
@@ -73,5 +89,4 @@ def number2color(numbers):
         np.ones(np.shape(numbers)),                 # saturation (always 1)
         np.abs(numbers),                            # lightness
     )).T
-    colors = hsl2rgb(hsl)
-    return colors
+    return hsl2rgb(hsl)
--- test_colors.py
+++ test_colors.py
@@ -5,22 +5,26 @@ Test for color modules
 
 import numpy as np
 import matplotlib.pyplot as plt
-from colors import color2number
+from colors import color2number, number2color
 
 def main():
     """
     main function here
     """
-    length = 100
+    length = 10
     colors = np.array([1/length * np.array((i, j, k))
                        for i in range(length + 1)
                        for j in range(length + 1)
                        for k in range(length + 1)])
 
     numbers = color2number(colors)
-    plt.figure(1)
+    plt.figure()
     plt.plot(np.real(numbers), np.imag(numbers), 'r+')
+    #colors = number2color(numbers)
+    #plt.figure()
+    #plt.plot()
     plt.show()
+
 
 if __name__ == "__main__":
     main()
